aadfac8b7f6072156bbb6874720c400cba560c09
Johnny Chow <Johnny.chow.19@gmail.com>
2015-12-26 08:37:04 +1000

    Added http request related classes.

---
+++ PresentationToolkit/PresentationToolkit.Core/NetPoxy/HttpClientProxy.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PresentationToolkit.Core.NetPoxy
+{
+    /// <summary>
+    /// Provides client http requests and responses.
+    /// </summary>
+    public sealed class HttpClientProxy : IDisposable
+    {
+        private readonly Uri uri;
+        private readonly HttpClient client;
+        private bool isDisposed;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="HttpClientProxy"/>.
+        /// </summary>
+        /// <param name="uri">The service uri.</param>
+        internal HttpClientProxy(Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri", nameof(uri));
+            }
+
+            this.uri = uri;
+
+            client = new HttpClient();
+            isDisposed = false;
+        }
+
+        /// <summary>
+        /// Requests the Http Get for the uri and reads the contents.
+        /// </summary>
+        /// <param name="uri">The service uri.</param>
+        /// <returns>The response contentss.</returns>
+        public static async Task<string> GetAsync(Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri", nameof(uri));
+            }
+
+            string content = null;
+            using (HttpClientProxy proxy = new HttpClientProxy(uri))
+            {
+                content = await proxy.GetAsync();
+            }
+
+            return content;
+        }
+
+        /// <summary>
+        /// Requests the Http Post for the uri and reads the contents.
+        /// </summary>
+        /// <param name="uri">The service uri.</param>
+        /// <param name="postContent">The <see cref="HttpContent"/> to post.</param>
+        /// <returns>The response contentss.</returns>
+        public static async Task<string> PostAsync(Uri uri, HttpContent postContent = null)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri", nameof(uri));
+            }
+
+            string content = null;
+            using (HttpClientProxy proxy = new HttpClientProxy(uri))
+            {
+                content = await proxy.PostAsync(postContent);
+            }
+
+            return content;
+        }
+
+        /// <summary>
+        /// Requests the Http Get for the uri and reads the contents.
+        /// </summary>
+        /// <returns>The response contentss.</returns>
+        public async Task<string> GetAsync()
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+
+                string content = await response.Content.ReadAsStringAsync();
+
+                return content;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Requests the Http Post for the uri and reads the contents.
+        /// </summary>
+        /// <param name="postContent">The <see cref="HttpContent"/> to post.</param>
+        /// <returns>The response contentss.</returns>
+        public async Task<string> PostAsync(HttpContent postContent = null)
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                response = await client.PostAsync(uri, postContent);
+                response.EnsureSuccessStatusCode();
+
+                string content = await response.Content.ReadAsStringAsync();
+
+                return content;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disposes the current instance.
+        /// </summary>
+        public void Dispose()
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            client.Dispose();
+        }
+    }
+}
---
+++ PresentationToolkit/PresentationToolkit.Core/NetPoxy/UriRequestExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace PresentationToolkit.Core.NetPoxy
+{
+    /// <summary>
+    /// Uri http Get/Post related extension methods.
+    /// </summary>
+    public static class UriRequestExtensions
+    {
+        /// <summary>
+        /// Requests the Http Get for the uri and deserialized the contents by JSON.
+        /// </summary>
+        /// <param name="uri">The service uri.</param>
+        /// <returns>The deserialized object by JSON from response contents.</returns>
+        public static async Task<T> GetAsync<T>(this Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri", nameof(uri));
+            }
+
+            string content  = await HttpClientProxy.GetAsync(uri);
+            T result = await Task.Run<T>(() => JsonConvert.DeserializeObject<T>(content));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Requests the Http Get for the uri and reads the contents.
+        /// </summary>
+        /// <param name="uri">The service uri.</param>
+        /// <returns>The response contents.</returns>
+        public static Task<string> GetAsync(this Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri", nameof(uri));
+            }
+
+            return HttpClientProxy.GetAsync(uri);
+        }
+
+        /// <summary>
+        /// Requests the Http Post for the uri and deserialized the contents by JSON.
+        /// </summary>
+        /// <param name="uri">The service uri.</param>
+        /// <param name="postContent">The <see cref="HttpContent"/> to post.</param>
+        /// <returns>The deserialized object by JSON from response contents.</returns>
+        public static async Task<T> PostAsync<T>(this Uri uri, HttpContent postContent = null)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri", nameof(uri));
+            }
+
+            string content = await HttpClientProxy.PostAsync(uri, postContent);
+            T result = await Task.Run<T>(() => JsonConvert.DeserializeObject<T>(content));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Requests the Http Get for the uri and reads the contents.
+        /// </summary>
+        /// <param name="uri">The service uri.</param>
+        /// <param name="postContent">The <see cref="HttpContent"/> to post.</param>
+        /// <returns>The response contentss.</returns>
+        public static Task<string> PostAsync(this Uri uri, HttpContent postContent = null)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri", nameof(uri));
+            }
+
+            return HttpClientProxy.PostAsync(uri, postContent);
+        }
+    }
+}
